97d0072ec6568dde5bb8356f23ddfab71ff34dd2
TheWen <m.penhouet@warfog.gg>
2023-11-21 10:25:53 +0100

    fix: a player can be added twice

--- src/model.ts
+++ src/model.ts
@@ -1,7 +1,7 @@
 export type Player = {
-  username: string
-  level: number
-}
+  username: string;
+  level: number;
+};
 
 // This array contains all the waiting players.
 // It is the data of your application.
@@ -9,40 +9,53 @@ export const queue: Player[] = [];
 export const activePlayers: Player[] = [];
 
 export function addToWaitingQueue(player: Player) {
- 
   if (!player.username || !player.level) {
     alert("Ho no :-o !");
     return false;
   }
-  for (let i = 0; i < queue.length; i++) {
-    if (queue[i].username === player.username) {
-      alert("Ho no no no no >:-( !");
-      return false;
-    }
-  }
 
+  // vérifier si le joueur est déjà actif
+  const isAlreadyActive = activePlayers.some(
+    (activePlayer) => activePlayer.username === player.username
+  );
+
+  // vérifier si le joueur est déjà dans la file d'attente
+  const isAlreadyInQueue = queue.some(
+    (queuedPlayer) => queuedPlayer.username === player.username
+  );
+
+  if (isAlreadyActive || isAlreadyInQueue) {
+    alert("Ho no no no no >:-( !");
+    return false;
+  }
+  // Ajouter le joueur à la file d'attente
   queue.push(player);
   return true;
 }
-
-
 
 // Ticket 3: create the removeFromWaitingQueue here
 export function removeFromWaitingQueue() {
   if (queue.length > 0) {
     const removedPlayer = queue.shift();
-    if (removedPlayer) {
-    addActivePlayer(removedPlayer);
+
+    // Vérifier si le joueur est déjà actif
+    const isAlreadyActive =
+      removedPlayer &&
+      activePlayers.some(
+        (activePlayer) => activePlayer.username === removedPlayer.username
+      );
+
+    if (removedPlayer && !isAlreadyActive) {
+      addActivePlayer(removedPlayer);
+      return true;
+    } else {
+      queue.push(removedPlayer!);
     }
-    return true;
-  } else {
-    return false;
   }
+
+  return false;
 }
 
- function addActivePlayer(player: Player) {
- activePlayers.push(player);
+function addActivePlayer(player: Player) {
+  activePlayers.push(player);
 }
-
-
-
